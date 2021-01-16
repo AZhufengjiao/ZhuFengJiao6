fbe87921548b23f0d8723b3ef0a86afcde92f967
AZhufengjiao <2231472240@qq.com>
2021-01-16 23:13:22 +0800

    2020.01.16

--- 黑马优购/pages/cates/cates.js
+++ 黑马优购/pages/cates/cates.js
@@ -5,14 +5,57 @@ Page({
    * 页面的初始数据
    */
   data: {
+    cateLeftData: [],
+    cateRightData: [],
+    height: 0,
+    currentIndex: 0
+  },
 
+  /**
+   * @name: 获取分类数据
+   */
+  handleCateGoryGet(index) {
+    wx.request({
+      url: 'https://www.uinav.com/api/public/v1/categories',
+      method: "GET",
+      success: (res) => {
+        console.log(res);
+        const {
+          meta,
+          message
+        } = res.data
+        if (meta.status == 200) {
+          this.setData({
+            cateLeftData: message,
+            cateRightData: message[index].children
+          })
+        }
+      },
+      fail: (err) => {
+        console.log(err);
+      }
+    })
+  },
+
+  /**
+   * 点击左侧菜单 更新右侧数据
+   */
+  handleCateClick: function (e) {
+    let index = e.currentTarget.dataset.index
+    this.handleCateGoryGet(index)
+    this.setData({
+      currentIndex:index
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.handleCateGoryGet(this.data.currentIndex)
+    this.setData({
+      height: (wx.getSystemInfoSync().windowHeight - 50) * 2
+    })
   },
 
   /**
